3986324c77b8b33aeaa368cfa0f882aa1db02253
Dalin Bac <bac00001@algonquinlive.com>
2023-04-13 20:50:15 -0400

    Add codes

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
+<script>
+  $(document).ready(function() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 100) {
+        $('.back-to-top').fadeIn();
+      } else {
+        $('.back-to-top').fadeOut();
+      }
+    });
 
+    $('.back-to-top').click(function(event) {
+      event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 800);
+    });
+  });
+</script>
 
